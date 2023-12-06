384b4ffcdea0d5e8b1e50cfcafa35585cb443767
courtouise sensei <testqa.artem1111@gmail.com>
2023-12-06 12:38:31 +0300

    adding sections_list

--- pars_2.py
+++ pars_2.py
@@ -7,8 +7,14 @@ with open("devcasino_scr1_var1.html", encoding="utf-8") as file:
 soup = BeautifulSoup(src, "lxml")
 
 sections = soup.find_all("div", class_= re.compile("wlc-sections"))
+sections_list = {}
 
 for section in sections:
     section_classes = section.get("class")
-    print(section_classes)
+    sections_list[section_classes[0]]= section_classes
+
+print(sections_list)
+
+    
+
 
